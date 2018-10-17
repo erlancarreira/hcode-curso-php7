45cc4fcd1c7c8ed88ed478f21525ae504c79fd7d
Erlan Carreira <erlancarreira@hotmail.com>
2018-10-17 01:43:44 -0300

    Inserir e alterar

--- dao/class/Usuario.class.php
+++ dao/class/Usuario.class.php
@@ -60,12 +60,8 @@ class Usuario
 		));
 
 		if(count($result) > 0) {
-            $row = $result[0];
-
-            $this->setIdusuario($row['idusuario']);
-            $this->setDeslogin($row['deslogin']);
-            $this->setDessenha($row['dessenha']);
-            $this->setDtcadastro(new DateTime($row['dtcadastro']));
+            
+            $this->setData($result[0]);
 		}
 	}
 
@@ -95,18 +91,56 @@ class Usuario
 		));
 
 		if(count($result) > 0) {
-            $row = $result[0];
-
-            $this->setIdusuario($row['idusuario']);
-            $this->setDeslogin($row['deslogin']);
-            $this->setDessenha($row['dessenha']);
-            $this->setDtcadastro(new DateTime($row['dtcadastro']));
+            
+            $this->setData($result[0]);
 		
 		} else {
 
             throw new Exception("Login e/ou senha invalidos.");
 		
 		}
+	}
+
+	public function setData($data)
+	{
+		$this->setIdusuario($data['idusuario']);
+        $this->setDeslogin($data['deslogin']);
+        $this->setDessenha($data['dessenha']);
+        $this->setDtcadastro(new DateTime($data['dtcadastro']));
+	}
+
+	public function insert() 
+	{
+		$sql = new Sql();
+
+		$result = $sql->select("CALL sp_usuarios_insert(:LOGIN, :PASS)", array(
+            ':LOGIN'=>$this->getDeslogin(),
+            ':PASS'=>$this->getDessenha() 
+		));
+
+		if(count($result) > 0) {
+			$this->setData($result[0]);
+		}
+	}
+
+	public function update($login, $pass)
+	{   
+		$this->setDeslogin($login);
+		$this->setDessenha($pass);
+		
+		$sql = new Sql();
+
+		$sql->query("UPDATE tb_usuarios SET deslogin = :LOGIN, dessenha = :PASS WHERE idusuario = :ID", array(
+			':LOGIN'=>$this->getDeslogin(),
+			':PASS'=>$this->getDessenha(),
+			':ID'=>$this->getIdusuario()
+		));
+	}
+
+	public function __construct($login = '', $senha = '') 
+	{
+		$this->setDeslogin($login);
+		$this->setDessenha($senha);
 	}
 
 	public function __toString()
--- dao/index.php
+++ dao/index.php
@@ -1,23 +1,32 @@
 <?php 
 
 require_once("config.php");
-
+//Carrega um usuario
 // $root = new Usuario();
-
 // $root->loadById(6);
-
 // echo $root;
 
+//Carrega uma lista de usuarios
 // $lista = Usuario::getList();
-
 // echo json_encode($lista);
-
 // $lista = Usuario::search('j');
-
 // echo json_encode($lista);
+
+// Carrega uma lista de usuarios buscando pelo login
+// $usuario = new Usuario();
+// $usuario->login("root", '!@#$');
+// echo $usuario;
+
+// Cria um novo usuario
+// $aluno = new Usuario("Fulano", 'senha123');
+// // $aluno->setDeslogin("aluno");
+// // $aluno->setDessenha("lasdas2656");
+// $aluno->insert();
+// echo $aluno;
 
 $usuario = new Usuario();
 
-$usuario->login("root", '!@#$');
+$usuario->loadById(9);
+$usuario->update("Professor", "1dwqd123");
 
 echo $usuario;
